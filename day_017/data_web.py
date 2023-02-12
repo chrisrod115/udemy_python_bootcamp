a70a3ffbdd6d8b2ecd24eaca238696598658d4b7
Christopher Rodriguez <chrisrod115@gmail.com>
2023-02-12 15:22:09 -0800

    day_017_complete
    Completed day 17 which included OOP while writing a modular quiz game. In order to get a better understanding of python programming.

---
+++ day_017/data_web.py
@@ -0,0 +1,37 @@
+question_data = [
+    {"category": "Entertainment: Video Games",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "The name of the main character of the video game \"The Legend of Zelda\", is Zelda.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "The song \"Megalovania\" by Toby Fox made its third appearence in the 2015 RPG "
+                 "\"Undertale\".",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "English new wave musician Gary Numan founded the video game development company Facepunch Studios "
+                 "in March 2009.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "The Mann Co. Store from Team Fortress 2 has the slogan \"We hire mercenaries and get in "
+                 "fights\".",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "Big the Cat is a playable character in \"Sonic Generations\".", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+                                      "question": "Tetris is the #1 best-selling video game of all-time.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "In \"Super Mario 3D World\", the Double Cherry power-up originated from a developer "
+                 "accidentally making two characters controllable.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "The ultimate phrase used by Pharah from Overwatch is: \"Justice rains from above!\"",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "In the game \"Subnautica\", a \"Spadefish\" will attack you.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "In \"Space Station 13\",  the station has a clown aboard it.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}]
--- day_017/main.py
+++ day_017/main.py
@@ -1,13 +1,20 @@
-from data import question_data
+# from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
+from data_web import question_data
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(q_text=question_text, q_answer=question_answer)
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+
+while quiz.still_has_question():
+    question = quiz.next_question()
+    if quiz.is_the_end():
+        quiz.is_the_end()
+
