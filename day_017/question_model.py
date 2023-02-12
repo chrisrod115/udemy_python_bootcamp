7cf3bee4172acc262192914e3eaddc1648851afe
Christopher Rodriguez <chrisrod115@gmail.com>
2023-02-12 12:23:01 -0800

    Day_17
    Quick update

--- day_017/question_model.py
+++ day_017/question_model.py
@@ -1,2 +1,8 @@
-    """This Class will generate a question
-    """
+"""This Class will generate a question
+"""
+
+
+class Question:
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
--- day_017/quiz_brain.py
+++ day_017/quiz_brain.py
@@ -1,2 +1,14 @@
-    """This Class will keep track of how well you are doing. 
-    """
+"""This Class will keep track of how well you are doing.
+"""
+
+
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+
+    def next_question(self):
+        current_quesiton = self.question_list[self.question_number]
+        self.question_number += 1
+        input(f"Q.{self.question_number}: {current_quesiton.text} (True/False): ")
